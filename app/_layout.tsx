5737200162fda2f064d8fe624cee27e92179129a
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-09-16 08:19:19 +0500

    setup auth.tsx and authgurard in layout,tsx to gurad the routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const isAuth = false;
+  const router = useRouter();
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace("/auth");
+    }
+  }, []);
+  return <>{children}</>;
+}
 export default function RootLayout() {
   return (
     <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <RouteGuard>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </RouteGuard>
     </>
   );
 }
