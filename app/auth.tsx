c725bf037898be99e2409e9aa2d7a40f2f44bce0
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-09-16 08:55:06 +0500

    feat: implement AuthScreen with sign in/sign up functionality and integrate react-native-paper

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,12 +1,72 @@
-import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 export default function AuthScreen() {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
     >
-      <View>
-        <Text>Create Account</Text>
+      <View style={styles.content}>
+        <Text style={styles.title} variant="headlineMedium">
+          {isSignUp ? "Create Account" : "Welcome Back"}
+        </Text>
+        <TextInput
+          style={styles.input}
+          label={"Email"}
+          placeholder="example@gmail.com"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          mode="outlined"
+        />
+        <TextInput
+          style={styles.input}
+          label={"Password"}
+          placeholder="example@gmail.com"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          mode="outlined"
+        />
+        <Button mode="contained" style={styles.button}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+        <Button
+          mode="text"
+          onPress={() => setIsSignUp(!isSignUp)}
+          style={styles.switchModeButton}
+        >
+          {isSignUp
+            ? "Already have an account? Sign In"
+            : "Don't have an account? Sign Up"}
+        </Button>
       </View>
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 22,
+    textAlign: "center",
+  },
+  input: {
+    marginBottom: 4,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
